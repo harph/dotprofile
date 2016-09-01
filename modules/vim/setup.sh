5db47874cbc498dc57c5aa47c53ae5daf14c2270
Harrington Joseph <hello@hjoseph.com>
2016-09-01 11:42:59 -0700

    Removed submodules and added them to gitignore

--- modules/vim/setup.sh
+++ modules/vim/setup.sh
@@ -1,5 +1,8 @@
 # Vim module
-echo `pwd`
-git clone git://github.com/roman/golden-ratio.git modules/vim/vim/bundle/golden-ratio
-git clone git://github.com/mattn/emmet-vim.git modules/vim/vim/bundle/emmet-vim
-git clone git://github.com/nvie/vim-flake8.git modules/vim/vim/bundle/vim-flake8
+BUNDLE_DIR="modules/vim/vim/bundle"
+
+git clone git://github.com/kien/ctrlp.vim.git "$BUNDLE_DIR/ctrlp.vim"
+git clone git://github.com/mattn/emmet-vim.git "$BUNDLE_DIR/emmet-vim"
+git clone git://github.com/roman/golden-ratio.git "$BUNDLE_DIR/golden-ratio"
+git clone git://github.com/nvie/vim-flake8.git "$BUNDLE_DIR/vim-flake8"
+git clone git://github.com/plasticboy/vim-markdown.git "$BUNDLE_DIR/vim-markdown"
